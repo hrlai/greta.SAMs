ad3d5018780fa44d4988ddc0635bebe691022c5a
Hao Ran Lai <hrlai.ecology@gmail.com>
2023-09-21 15:28:38 +1200

    better annotations and diagnostics

--- code/test.R
+++ code/test.R
@@ -1,3 +1,9 @@
+# This script aims to simulate data using ecomix, and then hopefully 
+# recover the parameters using a greta model
+
+
+# Simulate data following ecomix example ----------------------------------
+
 library(ecomix)
 
 set.seed(42)
@@ -5,7 +11,7 @@ sam_form <-
     stats::as.formula(paste0('cbind(', paste(paste0('spp', 1:20),
                                              collapse = ','), ")~x1+x2"))
 sp_form <- ~ 1
-beta <- matrix(c(-2.9, -3.6, -0.9, 1, .9, 1.9), 3, 2, byrow = TRUE)
+beta_true <- matrix(c(-2.9, -3.6, -0.9, 1, .9, 1.9), 3, 2, byrow = TRUE)
 dat <- data.frame(
     y = rep(1, 100),
     x1 = stats::runif(100, 0, 2.5),
@@ -17,7 +23,7 @@ simulated_data <-
         archetype_formula = sam_form,
         species_formula = sp_form,
         data = dat,
-        beta = beta,
+        beta = beta_true,
         family = "bernoulli"
     )
 
@@ -28,27 +34,57 @@ simulated_data <-
 
 library(greta)
 
+# data
 y <- as.matrix(simulated_data[, 1:20])
 x <- dat[, -1]
 
-n <- nrow(y)
-p <- ncol(y)
-m <- ncol(x)
-k <- 3
+# indices
+n <- nrow(y)  # number of sites
+p <- ncol(y)  # number of species
+m <- ncol(x)  # number of covariates
+k <- 3        # number of archetypes
 
+# slopes and intercepts
 beta  <- normal(0, 1, dim = c(k, m))
-beta0 <- normal(0, 1, dim = p) 
+beta0 <- normal(-1, 0.5, dim = p)   # using the known input in ecomix, for now
 
-z <- dirichlet(t(rep(0.1, k)), n_realisations = p)
+# probability of archetype assignment
+# not sure about the prior, using an uninformative prior like the ones in 
+# stable isotope models
+z <- dirichlet(t(rep(1, k)), n_realisations = p)
 
+# linear predictor
 eta <- x %*% t(beta) %*% t(z)
 eta <- sweep(eta, 2, beta0, "+")
 prob <- ilogit(eta)
 
+# likelihood and model
 distribution(y) <- bernoulli(prob)
 
 mod <- model(beta, beta0, z)
 
-o <- opt(mod)
-# draws <- mcmc(mod, chains = 1)
-# bayesplot::mcmc_trace(draws)
+# optimisation / draws
+o <- opt(mod, max_iterations = 1e3)   # try rerunning this a few times to see the "misalignment" shown below
+draws <- mcmc(mod)
+
+
+
+
+# Diagnostics -------------------------------------------------------------
+
+# compare coefficients from MAP estimation
+# the coefs don't always "align", because the archetype indices are not aligned?
+# e.g., sometimes the rows in betas are incorrect
+# Not sure if this is why they defined outcomes as an ordered variable in Stan?
+# See https://mc-stan.org/docs/stan-users-guide/summing-out-the-responsibility-parameter.html
+beta_true; o$par$beta
+plot(beta_true, o$par$beta)
+abline(0, 1)   
+
+# compare archetype classification
+attr(simulated_data, "SAMs")
+apply(o$par$z, 1, which.max)  # lousy way, sometimes misaligned
+
+# quite obvious switching between chains due to sampling of z
+library(bayesplot)
+mcmc_trace(draws) 
