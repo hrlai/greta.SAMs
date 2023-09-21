c3cb08b9de8adffadee4420b7151e9b0c8221330
Hao Ran Lai <hrlai.ecology@gmail.com>
2023-09-21 15:30:41 +1200

    annotate code

--- code/df.R
+++ code/df.R
@@ -1,9 +1,9 @@
 df <- expression(S * M)
 df_fmm <- expression(S * (K-1) + K * M)
 
-S <- 10
-M <- 1:10
-K <- 3
+S <- 10    # number of species
+M <- 1:10  # number of covariates
+K <- 3     # number of archetypes
 
 plot(M, eval(df), type = "b")
 lines(M, eval(df_fmm), type = "b", col = "steelblue")
